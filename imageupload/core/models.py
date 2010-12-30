4e9505f26187debb1e35261096b19bbc45d9fd98
Kevin Atienza <kevin.atienza@icannhas.com>
2010-12-30 18:03:44 +0800

    - Image model

--- imageupload/core/models.py
+++ imageupload/core/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from imagekit import models as ik_models
 
 class Image(ik_models.ImageModel):
+	caption = models.TextField(default = '')
+	client = models.TextField(default = '') # will be a many-to-many to Client model
+	display = models.BooleanField(default = True)
 	original_image = models.ImageField(upload_to = 'uploads')
+	creation_time = models.DateTimeField(auto_now_add = True)
+	edit_time = models.DateTimeField(auto_now = True)
 	
 	class IKOptions:
 		spec_module = 'core.ikspecs'
