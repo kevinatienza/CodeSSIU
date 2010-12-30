d4cd0452792a756f557c283745dd322ac838bdd1
Kevin Atienza <kevin.atienza@icannhas.com>
2010-12-30 13:46:18 +0800

    - Move loading of fileuploader css and js down to upload.html  - Working cropping and rotating

--- imageupload/core/models.py
+++ imageupload/core/models.py
@@ -5,6 +5,6 @@ class Image(ik_models.ImageModel):
 	original_image = models.ImageField(upload_to = 'uploads')
 	
 	class IKOptions:
-		#spec_module = 'core.ikspecs'
+		spec_module = 'core.ikspecs'
 		cache_dir = 'ikcache'
 		image_field = 'original_image'
--- imageupload/core/urls.py
+++ imageupload/core/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('imageupload.core.views',
 	(r'^list$', 'list'),
 	(r'^upload', 'upload'),
+	(r'^edit/(\d+)', 'edit'),
 )
