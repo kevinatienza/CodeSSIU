4f5f8f8f26db3283633f5f0d1bae1ed908fb9917
guwapoman <guwapoman@guwapoman-VirtualBox.(none)>
2011-01-05 17:24:16 +0800

    - Edit Dialog pop-up with tabs. - Added helper functions for magic filters. - Added sepia image filter

--- imageupload/core/ikspecs.py
+++ imageupload/core/ikspecs.py
@@ -1,6 +1,18 @@
 from imagekit.specs import ImageSpec
 from imagekit import processors
+from PIL import ImageOps
 import Image as PILImage
+
+# Helper functions
+def make_linear_ramp(white):
+	# putpalette expects [r,g,b,r,g,b,...]
+	ramp = []
+	r, g, b = white
+	for i in range(255):
+		ramp.extend((r*i/255, g*i/255, b*i/255))
+	return ramp
+
+# Processors
 
 class ResizeThumb(processors.Resize):
 	width = 150
@@ -13,8 +25,26 @@ class BAWer(processors.ImageProcessor):
 		img = PILImage.blend(img.convert('L').convert(img.mode), img, 0.0)
 		return img, fmt
 
+class Sepiaer(processors.ImageProcessor):
+	@classmethod
+	def process(cls, img, fmt, obj):
+		sepia = make_linear_ramp((255, 240, 192))
+		if img.mode != "L":
+			img = img.convert("L")
+		img = ImageOps.autocontrast(img)
+		img.putpalette(sepia)
+		img = img.convert("RGB")
+		return img, fmt
+
+# Image Specs
+
 class BlackAndWhite(ImageSpec):
+	access_as = 'black_and_white'
 	processors = [BAWer]
+	
+class Sepia(ImageSpec):
+	access_as = 'sepia'
+	processors = [Sepiaer]
 
 class Thumbnail(ImageSpec):
 	processors = [ResizeThumb]
